b1e63144305f9d598b4d53df062f8e17fdf7ea1b
ahmad saifullah <ahsai001@gmail.com>
2023-05-30 14:35:32 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World! We are here!",
+		})
+	})
+	r.Run(":8080")
+}
